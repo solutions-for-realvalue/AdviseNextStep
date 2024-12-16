ecf1c3b40497b6ff96f1cd5de3e33b8406e5832d
Arnaldo Sepulveda <natureuplift@protonmail.com>
2024-12-16 18:09:00 +1400

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import AddUserForm from './components/AddUserForm';
+import AdviceForm from './components/AdviceForm';
+import AdviceDisplay from './components/AdviceDisplay';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      {/* Navbar */}
+      <nav className="Navbar">
+        <div className="Navbar-title">
+          <h1>Welcome to Advisor</h1>
+        </div>
+        <div className="Navbar-login">
+          <AddUserForm />
+        </div>
+      </nav>
+
+      {/* Hero Section for Advice Input/Output */}
+      <div className="Hero">
+        <AdviceForm />
+        <AdviceDisplay />
+      </div>
+    </div>
+  );
+}
+
+export default App;
